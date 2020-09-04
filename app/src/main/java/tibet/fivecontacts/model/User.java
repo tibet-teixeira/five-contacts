a7f63eff1054b9649576a4638c8314b1a4f01315
Tibet Teixeira <tibetbrasil@gmail.com>
2020-09-03 23:59:47 -0300

    Starting project

---
+++ app/src/main/java/tibet/fivecontacts/model/User.java
@@ -0,0 +1,26 @@
+package tibet.fivecontacts.model;
+
+import java.io.Serializable;
+import java.lang.reflect.Array;
+
+public class User implements Serializable {
+    String nome;
+    String login;
+    String senha;
+    String email;
+
+
+    public User(String nome, String login, String password) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+}
